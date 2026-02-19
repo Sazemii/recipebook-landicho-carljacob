3bdc4efc0998c56dff6f2ac791cbf30b62c328d6
ThunderRabbit <zuzazibubabi@gmail.com>
2026-02-19 22:44:17 +0800

    added admin recipe control

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, RecipeIngredient, Ingredient
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+    list_display = ("name",)
+
