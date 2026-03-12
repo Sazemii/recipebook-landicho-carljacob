0d0e48076a89583ed63ec19ff62e6a0dda4a9f8b
ThunderRabbit <zuzazibubabi@gmail.com>
2026-03-12 15:09:45 +0800

    created forms and required login, updated urlpatterns and created the pages, also made a folder for storing the users' images

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeImageCreateView
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='recipes-list'),
+    path('recipe/add', RecipeCreateView.as_view(), name='recipe-create'),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-detail'),
+    path('recipe/<int:pk>/add_image',
+         RecipeImageCreateView.as_view(), name='recipe-add-image'),
 ]
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Recipe
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
 
 
 class RecipeListView(ListView):
@@ -11,3 +14,27 @@ class RecipeListView(ListView):
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'ledger/recipe_detail.html'
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    template_name = 'ledger/recipe_form.html'
+    form_class = RecipeForm
+
+
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    template_name = 'ledger/recipeimage_form.html'
+    form_class = RecipeImageForm
+
+    def form_valid(self, form):
+        form.instance.recipe = Recipe.objects.get(pk=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('recipe-detail', kwargs={'pk': self.kwargs['pk']})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recipe'] = Recipe.objects.get(pk=self.kwargs['pk'])
+        return context
